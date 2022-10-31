d2df12425c4f83cd92b8ae88c6240767e03f1055
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2022-10-31 10:43:51 +0200

    test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,7 +5,7 @@ import { HomePage } from './pages/HomePage';
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+        <h1>Hello, world!</h1>
       <p>Page info</p>
       <HomePage />
       <CatalogPage />
