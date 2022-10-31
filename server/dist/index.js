b753a25d79c01aadcfa6e35c76200e73f964cf51
Anna Kolmakova <annakolmakova@gmail.com>
2022-10-31 21:01:12 +0200

    fix

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,7 +2,7 @@ import express from "express";
 var PORT = 3000;
 var app = express();
 app.get("/test", function(req, res) {
-    res.status(200).send("Hello World 1234");
+    res.status(200).send("Hello World");
 });
 app.listen(PORT, function() {
     console.log("Server running on port ".concat(PORT));
