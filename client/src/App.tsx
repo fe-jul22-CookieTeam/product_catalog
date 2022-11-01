2d681d90723ed03f26408fb78b5ad7807afba310
Aleksandra Prognimak <aleksandra.prognimak@gmail.com>
2022-11-01 17:41:32 +0200

    add footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,19 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 import './App.scss';
+import { Footer } from './components/Footer';
 
 const App = () => {
   return (
-    <>
+    <div className='App'>
       <Header />
 
       <div className="main">
         <Outlet />
       </div>
-    </>
+
+      <Footer />
+    </div>
   );
 };
 
---
+++ client/src/components/Footer/FooterLogo/FooterLogo.tsx
@@ -0,0 +1,8 @@
+import { Link } from 'react-router-dom';
+
+
+export const FooterLogo = () => (
+  <div className="footer__logo">
+    <Link to="/" className="footer__logo-link" />
+  </div>
+);
