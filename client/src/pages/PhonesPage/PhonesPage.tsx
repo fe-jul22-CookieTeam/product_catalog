009fa4466cad55693888b3903120dd2cdfc974c6
Olga <lapochkaov@gmail.com>
2022-11-02 08:39:28 +0300

    Grid withaut grid

--- client/src/pages/PhonesPage/PhonesPage.tsx
+++ client/src/pages/PhonesPage/PhonesPage.tsx
@@ -3,12 +3,7 @@ import './PhonesPage.module.scss';
 import '../../styles/blocs/grid.scss';
 // import { Link } from 'react-router-dom';
 
-const Card = () => (
-  <div className="card">
-    Card
-  </div>
-);
-
+const Card = () => <div className="card">Card</div>;
 
 const MenuRout = () => (
   // <div className="menuRout">
@@ -20,31 +15,28 @@ const MenuRout = () => (
     <a href="#" className="menuRout__home">
       <img src="./images/Home" alt="Home" />
     </a>
-    <img src="../../images/chevron-R.svg" alt="Arrow" className="menuRout__arrow" />
-    <p className="menuRout__title">
-      Phones
-    </p>
+    <img
+      src="../../images/chevron-R.svg"
+      alt="Arrow"
+      className="menuRout__arrow"
+    />
+    <p className="menuRout__title">Phones</p>
   </div>
 );
 
 export const PhonesPage = () => (
   <>
-    
     <MenuRout />
     <div className="container">
       <div className="pagePhone">
-        <h1 className="pagePhone__title">
-          Mobile phones
-        </h1>
-        
-        <p className="pagePhone__count"> 
-          95 models
-        </p>
-        
+        <h1 className="pagePhone__title">Mobile phones</h1>
+
+        <p className="pagePhone__count">95 models</p>
 
         <div className="grid__container">
           <div className="container__row">
-            <div className="container__col-sm container__col-md"><Card />
+            <div className="container__col-sm container__col-md">
+              <Card />
               <Card />
               <Card />
               <Card />
@@ -55,12 +47,9 @@ export const PhonesPage = () => (
             </div>
           </div>
         </div>
-          
-
       </div>
-
     </div>
-    
+
     {/* <ItemCard /> */}
   </>
 );
