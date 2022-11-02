b19adaa344e0de6effdfe482fd6b9a1e818b5936
lizapolokhina <lizapolohina@gmail.com>
2022-11-02 09:17:53 +0100

    trying to commit to be able to pull afterwards

--- client/src/components/ItemCard/ItemCard.tsx
+++ client/src/components/ItemCard/ItemCard.tsx
@@ -1,7 +1,7 @@
 import './ItemCard.scss';
 
 export const ItemCard = () => {
-  const  item = {
+  const item = {
     image: '',
     title: 'Apple iPhone 14 Pro 128GB Silver (MQ023)',
     price: '999',
@@ -11,31 +11,23 @@ export const ItemCard = () => {
   };
 
   return (
-    <div className='item'>
-      <img 
-        src={item.image} 
-        alt={item.title} 
-        className={'item__image'}
-      />
-      <p className='item__title'>
-        {item.title}
-      </p>
+    <div className="item">
+      <img src={item.image} alt={item.title} className={'item__image'} />
+      <p className="item__title">{item.title}</p>
 
-      <div className='item__price'>
-        {`$${item.price}`}
-      </div>
+      <div className="item__price">{`$${item.price}`}</div>
 
       <div className="description">
         <div className="description__wrapper">
           <div className="description__title">Screen</div>
           <div className="description__value">{item.screenV}</div>
         </div>
-        
+
         <div className="description__wrapper">
           <div className="description__title">Capacity</div>
           <div className="description__value">{item.capacity}</div>
         </div>
-        
+
         <div className="description__wrapper">
           <div className="description__title">RAM</div>
           <div className="description__value">{item.ram}</div>
@@ -44,11 +36,8 @@ export const ItemCard = () => {
 
       <div className="buttons">
         <div className="buttons__addToCart">Add to cart</div>
-        <div className="buttons__addToFavourites"> 
-        ♥
-        </div>
+        <div className="buttons__addToFavourites">♥</div>
       </div>
-
     </div>
   );
 };
