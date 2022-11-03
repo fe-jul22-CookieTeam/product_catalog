55bc9298a74f924f4a18cb8a19aab713fcdf560d
lizapolokhina <lizapolohina@gmail.com>
2022-11-03 11:32:16 +0100

    added media query for the images

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from './components/Header';
 import './App.scss';
 import './styles/blocks/container.scss';
 import { Footer } from './components/Footer';
+import { ItemCard } from './components/ItemCard';
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <div className="main">
         <div className="container">
           <Outlet />
+          <ItemCard />
         </div>
       </div>
 
